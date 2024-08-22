49d93b36cb73cb6170c8b26a8f9fca5eef7dfa56
kagome <fu.taz.1102@gmail.com>
2024-08-22 18:05:55 +0900

    feat: ✨ #80 APIのセットアップとエラーハンドリング

---
+++ lgtm-data/error.tsx
@@ -0,0 +1,12 @@
+export const errorData = () => {
+  const options = {
+    width: 1200,
+    height: 630,
+  };
+  const jsx = (
+    <div tw="flex h-full w-full items-center justify-center bg-white text-7xl">
+      🚨 ERROR 🐈‍⬛
+    </div>
+  );
+  return { jsx, options };
+};
---
+++ src/app/api/v1/lgtm-images/route.tsx
@@ -0,0 +1,28 @@
+import { NextRequest } from "next/server";
+import { ImageResponse } from "next/og";
+import { errorData } from "@/lgtm-data/error";
+
+export const runtime = "edge";
+
+export async function GET(request: NextRequest) {
+  try {
+    const searchParams = request.nextUrl.searchParams;
+    const theme = searchParams.get("theme");
+
+    // 今後動的な値が必要になればここに追加し、getLgtmData()にpropsとして渡す
+    // const text = searchParams.get("text");
+
+    if (!theme) {
+      throw new Error("theme is required");
+    }
+
+    const { getLgtmData } = await import(`../../../../../lgtm-images/${theme}`);
+    const { jsx, options } = getLgtmData();
+
+    return new ImageResponse(jsx, options);
+  } catch (error) {
+    console.error(error);
+    const { jsx, options } = errorData();
+    return new ImageResponse(jsx, options);
+  }
+}
