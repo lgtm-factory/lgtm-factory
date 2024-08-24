08f362becbb125214556fdb1c72bd86e80748441
kagome <fu.taz.1102@gmail.com>
2024-08-24 23:56:58 +0900

    fix: 🛠️  #95 ステータスコードの追加

--- src/app/api/v1/lgtm-images/route.tsx
+++ src/app/api/v1/lgtm-images/route.tsx
@@ -17,7 +17,7 @@ export async function GET(request: NextRequest) {
       const { jsx, options } = errorData({
         errorMessage: "Theme is missing",
       });
-      return new ImageResponse(jsx, options);
+      return new ImageResponse(jsx, { ...options, status: 400 });
     }
 
     // テーマの読み込みに失敗した場合
@@ -28,7 +28,7 @@ export async function GET(request: NextRequest) {
       const { jsx, options } = errorData({
         errorMessage: `Failed to load theme: ${theme}`,
       });
-      return new ImageResponse(jsx, options);
+      return new ImageResponse(jsx, { ...options, status: 500 });
     }
 
     // 指定されたテーマが存在しない場合
@@ -36,17 +36,17 @@ export async function GET(request: NextRequest) {
       const { jsx, options } = errorData({
         errorMessage: `Invalid theme: ${theme}`,
       });
-      return new ImageResponse(jsx, options);
+      return new ImageResponse(jsx, { ...options, status: 404 });
     }
 
     const { jsx, options } = getLgtmData();
 
-    return new ImageResponse(jsx, options);
+    return new ImageResponse(jsx, { ...options, status: 200 });
   } catch (_) {
     // 予期せぬエラーが発生した場合
     const { jsx, options } = errorData({
       errorMessage: "An unexpected error occurred",
     });
-    return new ImageResponse(jsx, options);
+    return new ImageResponse(jsx, { ...options, status: 500 });
   }
 }
