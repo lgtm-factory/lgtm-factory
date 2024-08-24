b04f841170fa42ed5a818e9671d82e0ef5278748
kagome <fu.taz.1102@gmail.com>
2024-08-25 00:05:28 +0900

    fix: 🛠️  #95 errorMessageをstatusTextに変更

--- lgtm-data/error.jsx
+++ lgtm-data/error.jsx
@@ -2,10 +2,11 @@ export const errorData = (props) => {
   const options = {
     width: 1200,
     height: 630,
+    statusText: props.statusText,
   };
   const jsx = (
     <div tw="flex h-full w-full items-center justify-center bg-white text-7xl">
-      🚨 {props.errorMessage} 🐈‍⬛
+      🚨 {props.statusText} 🐈‍⬛
     </div>
   );
   return { jsx, options };
--- src/app/api/v1/lgtm-images/route.tsx
+++ src/app/api/v1/lgtm-images/route.tsx
@@ -15,7 +15,7 @@ export async function GET(request: NextRequest) {
     // テーマが入力されなかった場合
     if (!theme) {
       const { jsx, options } = errorData({
-        errorMessage: "Theme is missing",
+        statusText: "Theme is missing",
       });
       return new ImageResponse(jsx, { ...options, status: 400 });
     }
@@ -26,7 +26,7 @@ export async function GET(request: NextRequest) {
       ({ getLgtmData } = await import(`../../../../../lgtm-data/${theme}`));
     } catch (_) {
       const { jsx, options } = errorData({
-        errorMessage: `Failed to load theme: ${theme}`,
+        statusText: `Failed to load theme: ${theme}`,
       });
       return new ImageResponse(jsx, { ...options, status: 500 });
     }
@@ -34,7 +34,7 @@ export async function GET(request: NextRequest) {
     // 指定されたテーマが存在しない場合
     if (!getLgtmData) {
       const { jsx, options } = errorData({
-        errorMessage: `Invalid theme: ${theme}`,
+        statusText: `Invalid theme: ${theme}`,
       });
       return new ImageResponse(jsx, { ...options, status: 404 });
     }
@@ -45,7 +45,7 @@ export async function GET(request: NextRequest) {
   } catch (_) {
     // 予期せぬエラーが発生した場合
     const { jsx, options } = errorData({
-      errorMessage: "An unexpected error occurred",
+      statusText: "An unexpected error occurred",
     });
     return new ImageResponse(jsx, { ...options, status: 500 });
   }
