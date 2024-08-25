485bf77d66d0c12119c7190d39b5c90bbb7598a9
kagome <fu.taz.1102@gmail.com>
2024-08-25 19:29:50 +0900

    feat ✨ #79 画像のダウンロードボタンの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,15 @@ export default function Home() {
   }
   // 仮のurl
   const copyText = `[![LGTMeow](https://lgtm-images.lgtmeow.com/2022/05/10/23/8bb2a459-a0b5-4acc-970c-69ad8808f905.webp)](https://lgtmeow.com)`;
+  const url = "/api/v1/lgtm-images?theme=test";
   return (
     <main className="p-16">
       <Button onClick={() => copy(copyText)}>copy</Button>
+      <Button asChild>
+        <a href={url} download="lgtm-image">
+          download
+        </a>
+      </Button>
     </main>
   );
 }
