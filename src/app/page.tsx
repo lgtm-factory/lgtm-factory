829e082ed2a7371d33c44b7d92e4415d2b1bce29
kagome <fu.taz.1102@gmail.com>
2024-08-25 17:16:53 +0900

    fix: 🛠️  Buttonコンポーネントをshadcn/uiのものに置き換え

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,16 @@
 "use client";
-
-import Button from '../components/Button';
-
+import { Button } from "@/components/shadcn-ui/button";
 
 export default function Home() {
   function copy(copyText: string) {
     navigator.clipboard.writeText(copyText);
     alert("Copied!");
-  } 
+  }
   // 仮のurl
   const copyText = `[![LGTMeow](https://lgtm-images.lgtmeow.com/2022/05/10/23/8bb2a459-a0b5-4acc-970c-69ad8808f905.webp)](https://lgtmeow.com)`;
   return (
     <main className="p-16">
-      <Button
-        onClick={() => copy(copyText)}
-      >
-        copy!
-      </Button>
+      <Button onClick={() => copy(copyText)}>copy</Button>
     </main>
   );
 }
