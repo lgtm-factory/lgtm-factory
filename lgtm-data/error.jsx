e3ae0fdfd4933ac4022bebc84668ef5efcabc957
kagome <154225199+kagomen@users.noreply.github.com>
2024-08-26 14:51:21 +0900

    fix: 🛠️ #95 APIのエラーハンドリングを修正
    * fix: 🛠️  #95 APIのエラーハンドリングを修正
    
    * fix: 🛠️ テーマの読み込みに失敗した場合のエラーハンドリングを追加
    
    * fix: 🛠️  #95 ステータスコードの追加
    
    * fix: 🛠️  #95 errorMessageをstatusTextに変更
    
    * fix: 🛠️  #95 errorDataをgetErrorDataに変更
    
    * fix: 🛠️  #95 テーマ読み込みのエラーハンドリングの修正
    
    * fix: 🛠️ #95 エラーハンドリング全体の修正

--- lgtm-data/error.jsx
+++ lgtm-data/error.jsx
@@ -1,11 +1,12 @@
-export const errorData = () => {
+export const getErrorData = (props) => {
   const options = {
     width: 1200,
     height: 630,
+    statusText: props.statusText,
   };
   const jsx = (
     <div tw="flex h-full w-full items-center justify-center bg-white text-7xl">
-      🚨 ERROR 🐈‍⬛
+      🚨 {props.statusText} 🐈‍⬛
     </div>
   );
   return { jsx, options };
--- src/app/api/v1/lgtm-images/route.tsx
+++ src/app/api/v1/lgtm-images/route.tsx
@@ -1,28 +1,66 @@
 import { NextRequest } from "next/server";
 import { ImageResponse } from "next/og";
-import { errorData } from "@/lgtm-data/error";
+import { getErrorData } from "@/lgtm-data/error";
 
 export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const theme = searchParams.get("theme");
+
+  // 今後動的な値が必要になればここに追加し、getLgtmData()にpropsとして渡す
+  // const text = searchParams.get("text");
+
+  // テーマが入力されなかった場合
+  if (!theme) {
+    const { jsx, options } = getErrorData({
+      statusText: "Theme is missing",
+    });
+
+    return new ImageResponse(jsx, { ...options, status: 400 });
+  }
+
   try {
-    const searchParams = request.nextUrl.searchParams;
-    const theme = searchParams.get("theme");
+    const { getLgtmData } = await import(`../../../../../lgtm-data/${theme}`);
 
-    // 今後動的な値が必要になればここに追加し、getLgtmData()にpropsとして渡す
-    // const text = searchParams.get("text");
-
-    if (!theme) {
-      throw new Error("theme is required");
+    // ファイルは存在するが、中身を取得できない場合
+    if (typeof getLgtmData !== "function") {
+      throw new Error("Invalid module structure");
     }
 
-    const { getLgtmData } = await import(`../../../../../lgtm-data/${theme}`);
     const { jsx, options } = getLgtmData();
 
-    return new ImageResponse(jsx, options);
-  } catch (error) {
-    console.error(error);
-    const { jsx, options } = errorData();
-    return new ImageResponse(jsx, options);
+    return new ImageResponse(jsx, { ...options, status: 200 });
+  } catch (error: unknown) {
+    // テーマの読み込みに失敗した場合
+    if (
+      error instanceof Error &&
+      error.message === "Invalid module structure"
+    ) {
+      const { jsx, options } = getErrorData({
+        statusText: `Failed to load theme: ${theme}`,
+      });
+
+      return new ImageResponse(jsx, { ...options, status: 500 });
+    }
+
+    // 指定されたテーマが存在しない場合
+    if (
+      error instanceof Error &&
+      error.message.includes("Cannot find module")
+    ) {
+      const { jsx, options } = getErrorData({
+        statusText: `Invalid theme: ${theme}`,
+      });
+
+      return new ImageResponse(jsx, { ...options, status: 404 });
+    }
+
+    // 予期せぬエラーが発生した場合
+    const { jsx, options } = getErrorData({
+      statusText: "An unexpected error occurred",
+    });
+
+    return new ImageResponse(jsx, { ...options, status: 500 });
   }
 }
