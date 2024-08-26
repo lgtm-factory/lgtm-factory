ef6a14e25756a1b9e1b7a23f0251a693b14b3ffb
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-08-26 18:36:23 +0900

    fix: 🛠️ #126 画像データの変更に伴いインポートの方法を修正

--- src/app/api/v1/lgtm-images/route.tsx
+++ src/app/api/v1/lgtm-images/route.tsx
@@ -1,47 +1,66 @@
 import { NextRequest } from "next/server";
 import { ImageResponse } from "next/og";
-import { getErrorData } from "@/lgtm-data/error";
+import getErrorData from "@/lgtm-data/error";
+import { GetLgtmData } from "@/types/lgtm-data";
 
 export const runtime = "edge";
+
+export const DEFAULT_VALUES = {
+  emoji: "📦",
+  text: "LGTM Factory",
+  color: "#fcd34d",
+};
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const theme = searchParams.get("theme");
+  const text = searchParams.get("text");
+  const emoji = searchParams.get("emoji");
+  const color = searchParams.get("color");
 
-  // 今後動的な値が必要になればここに追加し、getLgtmData()にpropsとして渡す
-  // const text = searchParams.get("text");
+  const inputData = {
+    emoji: emoji || DEFAULT_VALUES.emoji,
+    text: text || DEFAULT_VALUES.text,
+    color: color || DEFAULT_VALUES.color,
+  };
 
   // テーマが入力されなかった場合
   if (!theme) {
-    const { jsx, options } = getErrorData({
-      statusText: "Theme is missing",
+    const { element, options } = getErrorData({
+      emoji: emoji || DEFAULT_VALUES.emoji,
+      color: color || DEFAULT_VALUES.color,
+      text: "Theme is missing",
     });
 
-    return new ImageResponse(jsx, { ...options, status: 400 });
+    return new ImageResponse(element, { ...options, status: 400 });
   }
 
   try {
-    const { getLgtmData } = await import(`../../../../../lgtm-data/${theme}`);
+    const { default: getLgtmData }: { default: GetLgtmData } = await import(
+      `../../../../../lgtm-data/${theme}`
+    );
 
     // ファイルは存在するが、中身を取得できない場合
     if (typeof getLgtmData !== "function") {
       throw new Error("Invalid module structure");
     }
 
-    const { jsx, options } = getLgtmData();
+    const { element, options } = getLgtmData(inputData);
 
-    return new ImageResponse(jsx, { ...options, status: 200 });
+    return new ImageResponse(element, { ...options, status: 200 });
   } catch (error: unknown) {
     // テーマの読み込みに失敗した場合
     if (
       error instanceof Error &&
       error.message === "Invalid module structure"
     ) {
-      const { jsx, options } = getErrorData({
-        statusText: `Failed to load theme: ${theme}`,
+      const { element, options } = getErrorData({
+        emoji: emoji || DEFAULT_VALUES.emoji,
+        color: color || DEFAULT_VALUES.color,
+        text: `Failed to load theme: ${theme}`,
       });
 
-      return new ImageResponse(jsx, { ...options, status: 500 });
+      return new ImageResponse(element, { ...options, status: 500 });
     }
 
     // 指定されたテーマが存在しない場合
@@ -49,18 +68,22 @@ export async function GET(request: NextRequest) {
       error instanceof Error &&
       error.message.includes("Cannot find module")
     ) {
-      const { jsx, options } = getErrorData({
-        statusText: `Invalid theme: ${theme}`,
+      const { element, options } = getErrorData({
+        emoji: emoji || DEFAULT_VALUES.emoji,
+        color: color || DEFAULT_VALUES.color,
+        text: `Invalid theme: ${theme}`,
       });
 
-      return new ImageResponse(jsx, { ...options, status: 404 });
+      return new ImageResponse(element, { ...options, status: 404 });
     }
 
     // 予期せぬエラーが発生した場合
-    const { jsx, options } = getErrorData({
-      statusText: "An unexpected error occurred",
+    const { element, options } = getErrorData({
+      emoji: emoji || DEFAULT_VALUES.emoji,
+      color: color || DEFAULT_VALUES.color,
+      text: "An unexpected error occurred",
     });
 
-    return new ImageResponse(jsx, { ...options, status: 500 });
+    return new ImageResponse(element, { ...options, status: 500 });
   }
 }
