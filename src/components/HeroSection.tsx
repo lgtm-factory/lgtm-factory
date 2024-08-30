31c4500813e165e6d37d7a762bb3c38f9e228479
kagome <154225199+kagomen@users.noreply.github.com>
2024-08-30 15:07:01 +0900

    style: 🎨 #133 ヒーローセクションの実装
    * style: 🎨 #133 ヒーローセクションの実装
    
    * style: 🎨 新しいデザインに修正
    
    * style: 🎨 #133 New LGTMボタンを実装

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import heroImage from "@/assets/logo.svg";
+
+export default function HeroSection() {
+  return (
+    <section className="flex items-end justify-between">
+      <div className="leading-none text-white">
+        <h1 className="flex items-baseline font-monoton">
+          <span className="text-5xl sm:text-[8vw]">LGTM</span>
+          <span className="ml-1 text-3xl sm:text-[calc(8vw*4/7)]">Factory</span>
+        </h1>
+        <p className="text-sm font-medium sm:text-[1.6vw]">
+          LGTM make code review more fun! 👍
+        </p>
+      </div>
+      <div className="relative hidden md:block">
+        <Image
+          src={heroImage}
+          alt="Hero image"
+          className="-z-10 w-[20vw] -translate-y-0.5 opacity-75"
+        />
+      </div>
+    </section>
+  );
+}
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,3 @@
 export const GITHUB_URL = "https://github.com/lgtm-factory/lgtm-factory";
+export const GITHUB_DESIGN_GUIDE_URL =
+  "https://github.com/lgtm-factory/lgtm-factory/blob/main/design-guide.md";
