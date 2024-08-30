67fab4ea8473f22bb231a6f36ccad3f02a97ec8e
kagome <fu.taz.1102@gmail.com>
2024-08-30 15:33:10 +0900

    style: 🎨 フッターの調整

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,7 @@
-import LogoImage from "@/components/LogoImage";
-
 export default function Footer() {
   return (
-    <footer className="sticky top-full flex h-32 items-center justify-end gap-8 px-9">
-      <LogoImage />
-      <span className="text-3xl font-medium">LGTM Factory</span>
+    <footer className="sticky top-full flex h-12 items-center justify-start gap-3 px-4 sm:px-6">
+      <span className="text-sm text-white">LGTM Factory</span>
     </footer>
   );
 }
