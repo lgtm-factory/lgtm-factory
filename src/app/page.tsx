f78693802a26aca61dd783fd64b79f6acae61d63
siso <149976293+siso001@users.noreply.github.com>
2024-08-31 21:13:13 +0900

    fix : 🛠️ #150 クリックイベントのエラーを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,9 @@
-"use client";
-
-import Image from "next/image";
-import logoImage from "@/assets/logo.svg";
 import EditSheet from "@/components/EditSheet";
-// import { Sheet } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="p-16">
+    <main className="container">
       <EditSheet />
-      <div className="text-4xl">
-        <Image src={logoImage} width={180} alt="logo image" />
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        {/* <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p>
-        <p className="font-monoton text-accent">LGTM Factory</p>
-        <p className="font-thin">LGTM良さそうだね</p>
-        <p className="font-bold">LGTM良さそうだね</p> */}
-      </div>
     </main>
   );
 }
--- src/components/EditSheet.tsx
+++ src/components/EditSheet.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/shadcn-ui/button";
 import {
   Sheet,
