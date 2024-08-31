a947584109cd78bdb2dc487753dc28971fa4efbc
kagome <154225199+kagomen@users.noreply.github.com>
2024-08-31 22:48:15 +0900

    fix: 🛠️ 削除し忘れた箇所の修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import logoImage from "@/assets/logo.svg";
 import HeroSection from "@/components/HeroSection";
 import NewLGTMButton from "@/components/NewLGTMButton";
 
-export default async function Home() {
+export default function Home() {
   function copy(copyText: string) {
     navigator.clipboard.writeText(copyText);
     alert("Copied!");
