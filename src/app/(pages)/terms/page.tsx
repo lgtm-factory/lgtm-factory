58eba41eebd13dfe9702ca7b039829349ca9ba2c
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-09-04 04:13:16 +0900

    refactor: 🔄 共通スタイルをコンポーネント化

--- src/app/(pages)/terms/page.tsx
+++ src/app/(pages)/terms/page.tsx
@@ -1,18 +1,39 @@
 import { siteMetadata } from "@/lib/constants";
 
+const Heading = ({ children }: { children: React.ReactNode }) => (
+  <h2 className="pt-4 text-2xl font-semibold">{children}</h2>
+);
+
+const Alert = ({ children }: { children: React.ReactNode }) => (
+  <p className="rounded-lg border border-gray-400 p-4">{children}</p>
+);
+
+const List = ({
+  children,
+  type,
+}: {
+  children: React.ReactNode;
+  type: "ul" | "ol";
+}) =>
+  type === "ul" ? (
+    <ul className="list-inside list-disc">{children}</ul>
+  ) : (
+    <ol className="list-inside list-decimal">{children}</ol>
+  );
+
 export default function Terms() {
   return (
     <>
       <div className="my-12 flex w-full">
-        <div className="w-full rounded-sm bg-white bg-opacity-50 p-16 leading-relaxed lg:w-3/4">
+        <div className="w-full space-y-4 rounded-sm bg-white/50 p-16 leading-relaxed lg:w-3/4">
           <h1 className="text-5xl font-semibold">ご利用について</h1>
-          <h2 className="mt-8 text-2xl font-semibold">はじめに</h2>
-          <p className="mt-4">
+          <Heading>はじめに</Heading>
+          <p>
             LGTM
             Factory（以下「本サービス」）のご利用にあたっては、以下の規定をご確認ください👋
           </p>
-          <p className="mt-4">要点：</p>
-          <ul className="mt-4 list-inside list-disc">
+          <p>要点：</p>
+          <List type="ul">
             <li>
               個人または商業的なプロジェクトで、無料で画像を使用できます。
             </li>
@@ -20,8 +41,8 @@ export default function Terms() {
             <li>
               画像自体をコンテンツや商品として複製・再配布・販売したりしないでください。
             </li>
-          </ul>
-          <p className="mt-4 rounded-lg border border-gray-400 p-4">
+          </List>
+          <Alert>
             また、利用規約の変更履歴は
             <a
               className="underline"
@@ -41,20 +62,18 @@ export default function Terms() {
               Issues
             </a>
             にてお問い合わせ下さい。
-          </p>
+          </Alert>
 
-          <h2 className="mt-8 text-2xl font-semibold">
-            1. 本サービスの利用目的
-          </h2>
-          <p className="mt-4">
+          <Heading>1. 本サービスの利用目的</Heading>
+          <p>
             本サービスは、HTMLとCSS、そしてフリー素材から生成された LGTM（Looks
             Good To Me）画像を提供するオープンソースのプロジェクトです。
             <br />
             本サービスの第一の目的は、ユーザーがコードレビューやコミュニケーションを楽しく効果的に行えるようにすることです。
           </p>
 
-          <h2 className="mt-8 text-2xl font-semibold">2. 画像の利用規定</h2>
-          <ol className="mt-4 list-inside list-decimal">
+          <Heading>2. 画像の利用規定</Heading>
+          <List type="ol">
             <li>
               ユーザーは、規約の範囲内であれば、本サービスで提供される画像を、個人、法人、商用、非商用問わず無料で使用することができます。
             </li>
@@ -63,10 +82,9 @@ export default function Terms() {
               Factory」としてください。
             </li>
             <li>著作権はLGTM Factoryに帰属し、放棄していません。</li>
-          </ol>
-
-          <h3 className="mt-4 font-semibold">画像の使用が禁止される場合：</h3>
-          <ul className="mt-4 list-inside list-disc">
+          </List>
+          <h3 className="font-semibold">画像の使用が禁止される場合：</h3>
+          <List type="ul">
             <li>画像自体をコンテンツ・商品として再配布・販売</li>
             <li>画像自体をメインのコンテンツとした使用</li>
             <li>公序良俗に反する目的での利用</li>
@@ -75,34 +93,33 @@ export default function Terms() {
             </li>
             <li>反社会的勢力や違法行為に関わる使用</li>
             <li>その他運営者が不適切と判断した場合</li>
-          </ul>
+          </List>
 
-          <h2 className="mt-8 text-2xl font-semibold">3. その他の禁止事項</h2>
-          <ol className="mt-4 list-inside list-decimal">
+          <Heading>3. その他の禁止事項</Heading>
+          <List type="ol">
             <li>
               過剰なリクエスト：サーバーに過度の負荷をかけるような大量のリクエストの送信
             </li>
             <li>
               迷惑行為：本サービスの運営を妨げる行為や、他のユーザーに迷惑をかける行為
             </li>
-          </ol>
-          <p className="mt-4">
+          </List>
+          <p>
             上記を確認した場合、特定のユーザーに対して予告なくアクセスを禁止・制限することがあります。
           </p>
 
-          <h2 className="mt-8 text-2xl font-semibold">4. 免責事項</h2>
-
-          <ol className="mt-4 list-inside list-decimal">
+          <Heading>4. 免責事項</Heading>
+          <List type="ol">
             <li>
               本サービスの使用及び閲覧は、ユーザーの責任において行うものとします。特定の目的への適合性や、エラーやバグがないことを保証するものではありません
             </li>
             <li>
               本サービスの利用により生じた損害について、運営者は一切の責任を負いません。
             </li>
-          </ol>
+          </List>
 
-          <h2 className="mt-8 text-2xl font-semibold">5. その他</h2>
-          <ol className="mt-4 list-inside list-decimal">
+          <Heading>5. その他</Heading>
+          <List type="ol">
             <li>
               運営者は、事前の通知なく本サービスの内容を変更したり、サービスを一時的または永続的に停止したりする権利を有します。
             </li>
@@ -112,7 +129,7 @@ export default function Terms() {
             <li>
               ユーザーは定期的に本規約を確認する責任があります。本サービスを利用し続けることで、変更後の規約にも同意したものとみなされます。
             </li>
-          </ol>
+          </List>
         </div>
 
         {/* TODO: 画像を配置 */}
