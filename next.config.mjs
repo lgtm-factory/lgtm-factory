e4926030e2ff74ff8bbecb1f308cb8b4af4589d2
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-09-12 20:24:58 +0900

    chore: 🧹#197 cloudflareへデプロイ用の設定の削除

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
   },
 };
-
-if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
-}
 
 export default nextConfig;
