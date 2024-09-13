e980b4e0a86855e2fb29afa55d4beff2da9badf4
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-09-13 18:55:33 +0900

    Revert "cloudflareからvercelに移行"

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
   },
 };
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
