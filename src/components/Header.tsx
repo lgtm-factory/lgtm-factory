e6ccfab1c1e7b59c8b3ad97f60792b819ea773eb
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-09-15 05:48:58 +0900

    refactor: 🔄 ヘッダーのスタイルを更新

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
-import { SiGithub } from "@icons-pack/react-simple-icons";
 import { Button } from "@/components/shadcn-ui/button";
-import LogoImage from "@/components/LogoImage";
+import logoImage from "../../public/logo.svg";
 import { siteMetadata } from "@/lib/constants";
+import Image from "next/image";
 
 function HeaderButton({ children }: { children: React.ReactNode }) {
   return (
@@ -18,20 +18,22 @@ function HeaderButton({ children }: { children: React.ReactNode }) {
 
 export default function Header() {
   return (
-    <header className="sticky left-0 top-0 z-50 flex h-12 w-full items-center justify-between px-4 font-medium sm:h-14 sm:px-6">
+    <header className="sticky left-0 top-0 z-50 flex h-12 w-full items-center justify-between px-4 font-medium sm:h-20 sm:px-24">
       <HeaderButton>
         <Link href="/">
-          <LogoImage />
+          <Image src={logoImage} alt="LGTM Factory logo" className="w-16" />
         </Link>
       </HeaderButton>
-
-      <Link href="/terms">License</Link>
-
-      <HeaderButton>
-        <a href={siteMetadata.GITHUB_URL} target="_blank">
-          <SiGithub size={24} />
-        </a>
-      </HeaderButton>
+      <div className="flex items-center gap-12 text-xl">
+        <HeaderButton>
+          <Link href="/terms">License</Link>
+        </HeaderButton>
+        <HeaderButton>
+          <a href={siteMetadata.GITHUB_URL} target="_blank">
+            GitHub
+          </a>
+        </HeaderButton>
+      </div>
     </header>
   );
 }
